b7b04ac173c9a27d424998782a569093f8aaa658
abhisha12 <147168481+abhisha12@users.noreply.github.com>
2023-10-06 15:36:58 +0000

    data types done

--- JavascriptBasics/test.js
+++ JavascriptBasics/test.js
@@ -1,1 +1,18 @@
-console.log('abhisha');
+//normal comparisons
+// console.log(2 >1);
+// console.log(1!=0);
+
+// comparison of different data types
+//console.log('3'> 2)
+
+//strict comparison
+// console.log(3 === '3')
+
+//Some unusual comparisons
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 0);
+/*Note: in JS, euality operator and comparison operator work differently, null==0 will be false but when comparison
+operator is used it converts null into a number which is 0, so second one ill be true
+
+
