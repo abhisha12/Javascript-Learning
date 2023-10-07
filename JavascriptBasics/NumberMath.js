0f6657698cd098554ebf840f3e214ff773d4a9b3
abhisha12 <147168481+abhisha12@users.noreply.github.com>
2023-10-07 13:52:22 +0000

    numbers done

---
+++ JavascriptBasics/NumberMath.js
@@ -0,0 +1,30 @@
+// let num =100;
+// let num1 = new Number(100);
+// console.log(num);
+// console.log(num1);
+//toString method of Number
+// let num2=num1.toString();
+// console.log(typeof num2);
+//tofixed 
+// let num3=200.38284
+// let result=num3.toFixed(3);
+// console.log(typeof result);
+
+//toPrecision
+// let var1 = 400.212194
+// let res =var1.toPrecision(1);
+// console.log(typeof res);
+
+//toExponential
+// let var2 = 5787.998;
+// let res1 = var2.toExponential();
+// console.log(res1);
+// console.log(typeof res1);
+
+//valueOf()
+// let var3 = 123;
+// let res2 = var3.valueOf();
+// console.log(res2);
+// console.log(typeof res2);
+
+//Note: ALL NUMBER PROTOTYPES CONVERT NUMBERS INTO STRING EXCEPT FOR valueOf() prototype
